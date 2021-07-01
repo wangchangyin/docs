373da0335ec9a7856978d946cf3abda226b0a34d
wangchangyin <2452952178@qq.com>
2021-07-01 13:14:43 +0800

    提交到git上面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,5 @@
-const moment = require('moment');
-moment.locale('zh-CN');
-
 module.exports = {
+    base:'/docs/',  //网站base
     title:'CY的博客',
     description:'CY的博客,王长印,个人博客',
     head: [
@@ -9,18 +7,6 @@ module.exports = {
       ['meta', { name: 'Keywords', href: 'CY的博客,王长印,个人博客' }],
       ['meta', { name: 'author', href: '王长印' }]
     ],
-
-    plugins: [
-      [
-        '@vuepress/last-updated',
-        {
-          transformer: (timestamp) => {
-            return moment(timestamp).format('llll'); // 2021年6月30日星期三 23:06
-          }
-        }
-      ]
-    ],
-
     themeConfig: {
       logo: '/assets/img/hero.png',
       lastUpdated: '更新时间', // string | boolean
@@ -33,14 +19,14 @@ module.exports = {
           items: [
             { text: 'Spring Boot', items: 
               [
-                { text: '服务注册', link: '/regist/' },
-                { text: '服务调用', link: '/fegin/' }
+                { text: '服务注册', link: '/about/' },
+                { text: '服务调用', link: '/about/' }
               ] 
             },
             { text: 'Spring Cloud', items: 
               [
-                { text: '服务注册', link: '/fegin/' },
-                { text: '服务调用', link: '/fegin/' }
+                { text: '服务注册', link: '/about/' },
+                { text: '服务调用', link: '/about/' }
               ] 
             }
           ]
@@ -59,13 +45,17 @@ module.exports = {
           'three', /* /bar/three.html */
           'four'   /* /bar/four.html */
         ],
-  
-        // fallback
-        '/': [
-          '',        /* / */
-          'contact', /* /contact.html */
-          'about'    /* /about.html */
-        ]
       }
-    }
+    },
+    plugins: [
+      [
+        '@vuepress/last-updated', // "上次更新"时间格式
+        {
+          transformer: (timestamp, lang) => {
+            const moment = require('moment') // https://momentjs.com/
+            return moment(timestamp).format('YYYY/MM/DD, hh:mm:ss');
+          }
+        }
+      ]
+    ]
   }
